5e0068a67d6a8d99a8634541acfbdeb382aaadbb
Eitan Peer <eitan.peer@cloudinary.com>
2016-11-02 11:57:35 +0200

    Fix image retrieval by tag with cache busting (nonce as created in constructor and not updated)

--- samples/photo_album_with_jquery/app/js/photo-album.service.ts
+++ samples/photo_album_with_jquery/app/js/photo-album.service.ts
@@ -12,25 +12,18 @@ import { Cloudinary } from './cloudinary.service';
 @Injectable()
 export class PhotoAlbum {
 
-  private photos: Observable<Photo[]>;
-
   constructor(private http: Http, private cloudinary: Cloudinary) {
+  }
+
+  getPhotos(): Observable<Photo[]> {
     // instead of maintaining the list of images, we rely on the 'myphotoalbum' tag
     // and simply retrieve a list of all images with that tag.
     let url = this.cloudinary.url('myphotoalbum', { format: 'json', type: 'list' })
       //cache bust
       + "?" + Math.ceil(new Date().getTime() / 1000);
 
-    this.photos = this.http
+    return this.http
       .get(url)
       .map((r: Response) => r.json().resources as Photo[]);
   }
-
-  getPhotos(): Observable<Photo[]> {
-    return this.photos;
-  }
-
-  // addPhoto(photo: Photo): void {
-  //   this.photosObserver.next([photo]);
-  // }
 }
